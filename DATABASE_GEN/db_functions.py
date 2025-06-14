51640811d16a1be498811c09db074e04a7d02b8d
Hikma Hamza <h.hamza@alustudent.com>
2025-06-14 17:55:02 +0000

    Update db_functions.py

--- DATABASE_GEN/db_functions.py
+++ DATABASE_GEN/db_functions.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 
 def convert_to_date(date_str: str) -> datetime:
-    """Converts a string in eg: '10 May 2024 4:30:58 PM' format to a datetime object."""
+    """ This converts a string in eg: '10 May 2024 4:30:58 PM' format to a datetime object."""
     return datetime.strptime(date_str, "%d %b %Y %I:%M:%S %p")
